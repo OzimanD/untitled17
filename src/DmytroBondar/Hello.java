10551679e3b1d3930ff707856e6e4abf003356b6
Dmytro Bondar <bondardima1990@gmail.com>
2024-05-26 02:51:16 +0300

    add tasks solutions

--- src/DmytroBondar/Hello.java
+++ src/DmytroBondar/Hello.java
@@ -1,5 +1,7 @@
 package DmytroBondar;
 
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -13,6 +15,10 @@ public class Hello {
         System.out.println(sumDigits(1234));
 
         System.out.println(reverseOrder(1234));
+
+        System.out.println(getNumberOfName());
+
+        System.out.println(getNumberOfDate());
     }
 
     //масив 3х3 зробити перестановку таким чином щоб рядки стали стовпчиками
@@ -47,14 +53,36 @@ public class Hello {
         }
         return res;
     }
+
+    //Завдання 3
+    //Створіть програму, яка виводить у консоль число імені, наприклад, на вхід Roman = R(18) + O(15) + M(13) + A(1) + N(14) = 61, на вихід 61.
+    public static int getNumberOfName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name:" );
+        String name = scanner.next().toUpperCase();
+        char[] nameChars = name.toCharArray();
+        int numberOfName = 0;
+        for (char nameChar : nameChars) {
+            numberOfName += nameChar - 64;
+        }
+        return numberOfName;
+    }
+
+    //Завдання 4
+    //Створіть програму, яка виводить у консоль число дати. Наприклад, на вхід 02/11/1995 = 0 + 2 + 1 + 1 + 1 + 9 + 9 + 5 = 28, на вихід = 28.
+    public static int getNumberOfDate() {
+        String date = "02/11/1995";
+        char[] dateChars = date.toCharArray();
+        int numberOfDate = 0;
+
+        for (char dateChar : dateChars) {
+            if (dateChar == '/') {
+                continue;
+            }
+            numberOfDate += Character.getNumericValue(dateChar);
+        }
+
+        return numberOfDate;
+    }
 }
-
-
-
-
-//Завдання 3
-//        Створіть програму, яка виводить у консоль число імені, наприклад, на вхід Roman = R(18) + O(15) + M(13) + A(1) + N(14) = 61, на вихід
-//61.
-//Завдання 4
-//        Створіть програму, яка виводить у консоль число дати. Наприклад, на вхід 02/11/1995 = 0 + 2 + 1 + 1 + 1 + 9 + 9 + 5 = 28, на вихід = 28.
 
